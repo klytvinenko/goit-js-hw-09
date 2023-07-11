906093b932da26c1ed26c7dbe85ae23d1901eb74
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2023-07-11 11:28:07 +0300

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let idInt = null;
+console.log(startBtn);
+console.log(stopBtn);
 
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+
+function onStart() {
+    startBtn.disabled = true;
+    idInt = setInterval (() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+   
+}
+
+function onStop() {
+    clearInterval(idInt);
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
