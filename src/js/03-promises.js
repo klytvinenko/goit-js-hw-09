4cc0a338b0bce27ee6634948029f9a10b0c3d978
klytvinenko <58306149+klytvinenko@users.noreply.github.com>
2023-07-11 16:37:44 +0300

    third

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,106 @@
+import Notiflix from 'notiflix';
+console.log(Notiflix);
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise ((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({position, delay});
+      } else {
+        // Reject
+        reject({position, delay});
+      }
+    }, delay)
+
+  });
 }
+
+const form = document.querySelector('form');
+let positionV = 0;
+let delayV = 0;
+
+form.addEventListener('submit', onPromise);
+
+function onPromise(evt) {
+  evt.preventDefault();
+
+  const delayInpt = document.querySelector('input[name="delay"]');
+  const stepInpt = document.querySelector('input[name="step"]');
+  const amountInpt = document.querySelector('input[name="amount"]');
+
+  const firstStep = Number(delayInpt.value);
+  const step = Number(stepInpt.value);
+  const amount = Number(amountInpt.value);
+
+
+  for(let i = 0; i <= amount; i++) {
+    const position = i + 1;
+    const delay = firstStep + (i * step);
+      
+    createPromise(position, delay)
+      .then(({position,delay}) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({position, delay}) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      })
+  }
+
+
+  form.reset();
+}
+
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// const form = document.querySelector('.form');
+// form.addEventListener('submit', function (event) {
+//   event.preventDefault();
+
+//   const delayInput = document.querySelector('input[name="delay"]');
+//   const stepInput = document.querySelector('input[name="step"]');
+//   const amountInput = document.querySelector('input[name="amount"]');
+
+//   const firstDelay = parseInt(delayInput.value);
+//   const step = parseInt(stepInput.value);
+//   const amount = parseInt(amountInput.value);
+
+//   for (let i = 0; i < amount; i++) {
+//     const position = i + 1;
+//     const delay = firstDelay + i * step;
+
+//     createPromise(position, delay)
+//       .then(({ position, delay }) => {
+//         Notiflix.Notify.success(
+//           `✅ Fulfilled promise ${position} in ${delay}ms`
+//         );
+//       })
+//       .catch(({ position, delay }) => {
+//         Notiflix.Notify.failure(
+//           `❌ Rejected promise ${position} in ${delay}ms`
+//         );
+//       });
+//   }
+
+//   form.reset();
+// });
